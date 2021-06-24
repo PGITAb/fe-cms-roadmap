07f0e24165834da63243acc6a419d096a634cdfb
Ming Choi <mingchoi.na@gmail.com>
2021-06-24 12:20:29 +0800

    Fix ROL roadmap UI

--- src/components/Roadmap/fns/renderRoRoadmap.tsx
+++ src/components/Roadmap/fns/renderRoRoadmap.tsx
@@ -110,7 +110,9 @@ const renderRoRoadmap = (statistic: any, lang: string): React.ReactNode => {
   let balls: any = [];
   if (statistic.last20Rounds !== undefined) {
     balls = statistic.last20Rounds.map((r: any) =>
-      <ROLBall value={r.v} tag={r.odds} />
+      <div className="roadmap-ro-ball-slot">
+        <ROLBall value={r.v} tag={r.odds} />
+      </div>
     );
     balls.push(<div style={{ height: "16px" }}></div>);
   } else if (statistic.last36Rounds !== undefined) {
